08ac0b1463ecac1e5491c91d16c3a73ca5b06fec
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-14 09:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ class Driver{
 
 class PickupLocation{
   constructor(name,age,experience){
-    this.name=name
-    this.age=age
-    this.experience=experience
+    this.address=name
+    this.city=age
+    
   }
 }
 
