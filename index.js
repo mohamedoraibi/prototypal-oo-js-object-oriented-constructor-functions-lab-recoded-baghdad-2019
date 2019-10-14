e31f8c01729d4077d0a3d9e96f9d5ff0b5be7274
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-14 09:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-class 
-+ Scooter with `year`, `color`, and `model` properties
+class Scooter{
+  constructor(year,color,model){
+    this.year=year
+    this.color=color
+    this.model=model
+  }
+}
+
+
+class Driver{
+  constructor(name,age,experience){
+    this.name=name
+    this.age=age
+    this.experience=experience
+  }
+}
 + Driver with `name`, `age`, and `experience` properties
 + PickupLocation with `address` and `city` properties
